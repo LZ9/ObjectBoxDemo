877b747dac48b528e41a9087482cfc22e6e40b3e
LZ9 <lz.mms@foxmail.com>
2019-12-16 15:34:46 +0800

    添加Rx调用用例

--- app/src/main/java/com/lodz/android/objectboxdemo/ui/rx/RxActivity.kt
+++ app/src/main/java/com/lodz/android/objectboxdemo/ui/rx/RxActivity.kt
@@ -4,11 +4,19 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.widget.EditText
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.button.MaterialButton
 import com.lodz.android.corekt.anko.bindView
+import com.lodz.android.corekt.anko.toastShort
 import com.lodz.android.objectboxdemo.R
+import com.lodz.android.objectboxdemo.db.dao.ObjectBoxImpl
+import com.lodz.android.objectboxdemo.db.table.NoteTableBean
+import com.lodz.android.objectboxdemo.ui.list.NoteListAdapter
 import com.lodz.android.pandora.base.activity.BaseActivity
+import com.lodz.android.pandora.rx.subscribe.observer.BaseObserver
+import com.lodz.android.pandora.rx.utils.RxUtils
+import io.reactivex.Observable
 
 /**
  * RxJava调用
@@ -18,7 +26,7 @@ import com.lodz.android.pandora.base.activity.BaseActivity
 class RxActivity : BaseActivity() {
 
     companion object {
-        fun start(context: Context){
+        fun start(context: Context) {
             val intent = Intent(context, RxActivity::class.java)
             context.startActivity(intent)
         }
@@ -27,12 +35,24 @@ class RxActivity : BaseActivity() {
     private val mContentEdit by bindView<EditText>(R.id.content_edit)
     private val mInsertBtn by bindView<MaterialButton>(R.id.insert_btn)
     private val mRecyclerView by bindView<RecyclerView>(R.id.recycler_view)
+    private lateinit var mAdapter: NoteListAdapter
 
     override fun getLayoutId(): Int = R.layout.activity_db
 
     override fun findViews(savedInstanceState: Bundle?) {
         super.findViews(savedInstanceState)
-        getTitleBarLayout().setTitleName(R.string.main_rx)
+        getTitleBarLayout().setTitleName(R.string.main_normal)
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        mAdapter = NoteListAdapter(getContext())
+        val layoutManager = LinearLayoutManager(getContext())
+        layoutManager.orientation = RecyclerView.VERTICAL
+        mRecyclerView.layoutManager = layoutManager
+        mAdapter.onAttachedToRecyclerView(mRecyclerView)// 如果使用网格布局请设置此方法
+        mRecyclerView.setHasFixedSize(true)
+        mRecyclerView.adapter = mAdapter
     }
 
     override fun onClickBackBtn() {
@@ -40,8 +60,89 @@ class RxActivity : BaseActivity() {
         finish()
     }
 
+    override fun setListeners() {
+        super.setListeners()
+
+        mInsertBtn.setOnClickListener {
+            val content = mContentEdit.text.toString()
+            if (content.isEmpty()){
+                toastShort(R.string.main_input_hint)
+                return@setOnClickListener
+            }
+            mContentEdit.setText("")
+            insertData(content)
+        }
+
+        mAdapter.setOnClickUpdateListener { bean ->
+            updateData(bean.id)
+        }
+
+        mAdapter.setOnClickDeleteListener { bean ->
+            deleteData(bean)
+        }
+
+    }
+
+    /** 插入数据，内容[content] */
+    private fun insertData(content: String) {
+        Observable.just(content)
+            .flatMap {
+                ObjectBoxImpl.get().addNote(it).rx()
+            }
+            .flatMap {
+                ObjectBoxImpl.get().getNoteList().rx()
+            }
+            .compose(RxUtils.ioToMainObservable())
+            .compose(bindDestroyEvent())
+            .subscribe(BaseObserver.action({ list ->
+                mAdapter.setData(list)
+                mAdapter.notifyDataSetChanged()
+            }))
+    }
+
+    /** 更新数据，数据[id] */
+    private fun updateData(id: Long) {
+        Observable.just(id)
+            .flatMap {
+                ObjectBoxImpl.get().updateNote(it).rx()
+            }
+            .flatMap {
+                ObjectBoxImpl.get().getNoteList().rx()
+            }
+            .compose(RxUtils.ioToMainObservable())
+            .compose(bindDestroyEvent())
+            .subscribe(BaseObserver.action({ list ->
+                mAdapter.setData(list)
+                mAdapter.notifyDataSetChanged()
+            }))
+    }
+
+    /** 更新数据，数据[bean] */
+    private fun deleteData(bean: NoteTableBean) {
+        Observable.just(bean)
+            .flatMap {
+                ObjectBoxImpl.get().removeNote(it).rx()
+            }
+            .flatMap {
+                ObjectBoxImpl.get().getNoteList().rx()
+            }
+            .compose(RxUtils.ioToMainObservable())
+            .compose(bindDestroyEvent())
+            .subscribe(BaseObserver.action({ list ->
+                mAdapter.setData(list)
+                mAdapter.notifyDataSetChanged()
+            }))
+    }
+
     override fun initData() {
         super.initData()
-        showStatusCompleted()
+        ObjectBoxImpl.get().getNoteList().rx()
+            .compose(RxUtils.ioToMainObservable())
+            .compose(bindDestroyEvent())
+            .subscribe(BaseObserver.action({ list ->
+                mAdapter.setData(list)
+                mAdapter.notifyDataSetChanged()
+                showStatusCompleted()
+            }))
     }
 }
